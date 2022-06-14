5fe90cdccc6d47c91a567174912fc6e5b2079487
Helmut Joaquín Pfeffer García <helmutjpfeffer@gmail.com>
2022-06-14 15:16:16 +0200

    Update Code.java

--- codes/Code.java
+++ codes/Code.java
@@ -1,5 +1,35 @@
 package codes;
+
+import io.*;
+
 public abstract class Code {
     public abstract void encode();
+
     public abstract void decode();
+
+    protected void manageChar(char ch) {
+    }
+
+    protected void printLine() {
+        Out.println();
+    }
+
+    /**
+     * Loops through all the characters and jumps the line unless printLine() is
+     * override
+     */
+    public void loopEveryChar() {
+        String linea = In.readLine();
+
+        while (In.done()) {
+
+            for (char ch : linea.toCharArray()) {
+
+                manageChar(ch);
+            }
+
+            linea = In.readLine();
+            printLine();
+        }
+    }
 }
